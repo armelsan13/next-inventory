755b291a1bd0909d1480fc730fbbe626c59e3c69
ArmelMBP <armelcabaluna@gmail.com>
2023-10-31 03:46:43 +0800

    add type to tag of tags

--- app/inventory/components/EquipmentItem.tsx
+++ app/inventory/components/EquipmentItem.tsx
@@ -3,14 +3,28 @@ import Image from 'next/image'
 import Link from 'next/link'
 
 
-type EquipmentItem = {
+type Tag = {
+    id: string,
+    title: string
+}
+
+type Equipment = {
     id: string,
     title: string,
     description: string | null,
     tags: any
 }
 
-export function EquipmentItem({ id, title, description, tags }: EquipmentItem) {
+export function EquipmentItem({ id, title, description, tags }: Equipment) {
+
+    const tagsList = tags ? (
+        <div className="card-actions justify-end">
+                    {tags.map((tag : Tag) => (
+                        <div key={tag.id} className="badge badge-accent px-3">{tag.title}</div>
+                    ))}
+                </div>
+    ) : null
+
     return (
         <div className="card w-auto bg-base-100 shadow-xl">
             <figure className='py-5'><Image src={defaultImage} alt={title} width={100} height={120} /></figure>
@@ -25,11 +39,7 @@ export function EquipmentItem({ id, title, description, tags }: EquipmentItem) {
                     <div className="flex-none"><Link href='#' className='btn btn-outline btn-xs'>Edit</Link></div>
                 </div>            
                 <p>{description}</p>
-                <div className="card-actions justify-end">
-                    {tags.map((tag : any) => (
-                        <div key={tag.id} className="badge badge-accent px-3">{tag.title}</div>
-                    ))}
-                </div>
+                {tagsList}
             </div>
         </div>
     )
