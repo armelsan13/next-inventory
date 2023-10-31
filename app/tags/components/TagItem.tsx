01cf090eb7331ee17c1fc5fb2e85a73f078ea24d
ArmelMBP <armelcabaluna@gmail.com>
2023-10-31 12:50:36 +0800

    ---

--- app/tags/components/TagItem.tsx
+++ app/tags/components/TagItem.tsx
@@ -7,7 +7,7 @@ type TagItemProps = {
 
 export function TagItem({id, title} : TagItemProps) {
     return (
-        <div className="border-solid border-2 border-indigo-900 p-3 rounded-md">
+        <div className="border-solid border-2 border-indigo-900 p-3 rounded-md mb-3">
             <div className="w-full flex flex-row">
                 <div className="flex-1">{title}</div>
                 <div className="flex-none">
